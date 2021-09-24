b0ec1bdb07243acc374c3c4f1710c1215943458c
isiezz_ <83096990+isiezz@users.noreply.github.com>
2021-09-24 00:31:33 +0000

    Tentando subir a API no Heroku

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-def main():
-  return "Hello World!"
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/api/calcular", methods=['GET', 'POST'])
+def api_calcular():
+    numero1 = request.form.get('numero1')
+    numero2 = request.form.get('numero2')
+    try:
+        int(numero1)
+    except ValueError:
+        return jsonify({'erro': f'Nao é um valor inteiro o valor digitado. ({numero1})'})
+    try:
+        int(numero2)
+    except ValueError:
+        return jsonify({'erro': f'Nao é um valor inteiro o valor digitado. ({numero1})'})
+    if numero1 and numero2:
+        resultado = int(numero1) + int(numero2)
+    else:
+        resultado = "Envie dois numeros para somar."
+    return jsonify({'resultado': resultado})
+
+@app.route("/")
+def hello_world():
+    data = {
+      "nome": "Enderson Menezes",
+      "idade": "25",
+      "cidade": "Marialva/PR",
+    }
+    return render_template("index.html", data=data)
+
+if __name__ == "__main__":
+	app.run(debug=True)
